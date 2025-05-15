adfc4164deb566727534de6f6f0301ed0a381338
ChemzRegaton <160089008+ChemzRegaton@users.noreply.github.com>
2025-05-15 17:54:48 +0800

    Update additionalInfo.jsx

--- src/user/components/additionalInfo.jsx
+++ src/user/components/additionalInfo.jsx
@@ -222,7 +222,7 @@ function AdditionalInfo({ onClose, onProfileUpdated, initialProfileData }) {
                         onChange={handleChange}
                     />
                 </section >
-                    <section style={{display: "flex", flexDirection: "row"}}>
+                    <section style={{display: "flex", flexDirection: "row", height: "10vh"}}>
                             <button className='submit' disabled={isSubmitting}>
                         {isSubmitting ? 'Submitting...' : 'Submit'}
                     </button>
